b8ce6389b1f3c57d181ef069b741716b7882eb14
jar2004 <jar2004@hw.ac.uk>
2023-09-22 12:16:59 +0100

    Added Codewars folder for practicing/learning JS

---
+++ CodewarsJS/camelCase.js
@@ -0,0 +1,20 @@
+function toCamelCase(str){
+  if (str == ''){
+    return '';
+  }
+  else 
+    var splitString = str.split("");
+    console.log(splitString);
+   for (var i=0;i<splitString.length - 1;i++){
+     
+     if (splitString[i] === "_"  || splitString[i] === '-'){
+       splitString[i+1] = splitString[i+1].toUpperCase();
+       splitString[i] = "";
+       console.log(i);
+     }  //endif
+     
+   }  //endfor
+  const result = splitString.join("");
+  console.log(result);
+  return result
+}
