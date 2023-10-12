d54141484d377de364445720c753887f15ec7127
jar2004 <jar2004@hw.ac.uk>
2023-10-12 10:26:27 +0100

    Started Pong

---
+++ Pong/pong.js
@@ -0,0 +1,5 @@
+/* grabbing elements into variables */
+let paddle_1 = document.querySelector('.paddle_1');
+let paddle_2 = document.querySelector('.paddle_2');
+let board = document.querySelector('.board');
+let ball = document.querySelector('.ball');
